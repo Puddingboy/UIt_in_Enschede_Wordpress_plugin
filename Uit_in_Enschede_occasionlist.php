8e1cd565785b1cde3d703217ab0d6ede4dc8d82b
Ruben Damink <ruben.damink@gmail.com>
2013-07-12 21:21:24 +0200

    Small description edit

--- Uit_in_Enschede_occasionlist.php
+++ Uit_in_Enschede_occasionlist.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Uit in Enschede occasion list
 Version: 0.1
-Description: Retrieves occasions from uitinenschede.nl and displays them in a list.
+Description: Retrieves information about occasions from http://www.uitinenschede.nl/ and displays them in a list.
 Author: Ruben Damink
 Author URI: http://rubendamink.nl
 */
