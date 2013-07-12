3fc8e6e02efe95e513f1ce781088a4d1d3ffd525
Ruben Damink <ruben.damink@gmail.com>
2013-07-12 21:16:49 +0200

    PHP file created
    Comment block added for wordpress recognition.

---
+++ Uit_in_Enschede_occasionlist.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Uit in Enschede occasion list
+Version: 0.1
+Description: Retrieves occasions from uitinenschede.nl and displays them in a list.
+Author: Ruben Damink
+Author URI: http://rubendamink.nl
+*/
+?>
